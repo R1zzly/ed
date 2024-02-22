4a8f0fe3f1f99c4e4933a3ffbc11992bf40f519d
R1zzly <93976440+R1zzly@users.noreply.github.com>
2024-02-22 12:12:21 +0600

    Add files via upload

--- tgroupBuy/lib/main.dart
+++ tgroupBuy/lib/main.dart
@@ -1,25 +1,36 @@
 import 'package:flutter/material.dart';
-import 'widgets/homePage.dart' show HomePageModel, HomePageWidget;
+import 'package:provider/provider.dart';
+import 'package:tgroupbuy/provider/user_provider.dart';
+import 'package:tgroupbuy/routes.dart';
+import 'package:tgroupbuy/widgets/signIn.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        brightness: Brightness.light,
-        primaryColor: Colors.blue,
-        fontFamily: 'Georgia',
-        textTheme: TextTheme(
-          headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
-          headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
-          bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+    return ChangeNotifierProvider.value(
+      value: UserProvider(),
+      child: MaterialApp(
+        title: 'Together',
+        theme: ThemeData(
+          brightness: Brightness.light,
+          primaryColor: Colors.blue,
+          fontFamily: 'Georgia',
+          textTheme: const TextTheme(
+            displayLarge: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+            titleLarge: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+            bodyMedium: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+          ),
         ),
+        home:
+            const SignInWidget(),
+           onGenerateRoute: onGenerateRoutes,
       ),
-      home: HomePageWidget(), // Set your HomePageWidget as the home screen
     );
   }
 }
