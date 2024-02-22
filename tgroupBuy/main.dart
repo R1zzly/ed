9533f65610e90f3f862c72ec18ff2f8ec71305f6
R1zzly <93976440+R1zzly@users.noreply.github.com>
2024-02-22 12:07:07 +0600

    Add files via upload

--- tgroupBuy/main.dart
+++ tgroupBuy/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tgroupbuy/provider/user_provider.dart';
+import 'package:tgroupbuy/routes.dart';
 import 'package:tgroupbuy/widgets/signIn.dart';
 
 void main() {
@@ -10,20 +13,24 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        brightness: Brightness.light,
-        primaryColor: Colors.blue,
-        fontFamily: 'Georgia',
-        textTheme: const TextTheme(
-          displayLarge: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
-          titleLarge: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
-          bodyMedium: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+    return ChangeNotifierProvider.value(
+      value: UserProvider(),
+      child: MaterialApp(
+        title: 'Together',
+        theme: ThemeData(
+          brightness: Brightness.light,
+          primaryColor: Colors.blue,
+          fontFamily: 'Georgia',
+          textTheme: const TextTheme(
+            displayLarge: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+            titleLarge: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+            bodyMedium: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+          ),
         ),
+        home:
+            const SignInWidget(),
+           onGenerateRoute: onGenerateRoutes,
       ),
-      home:
-          const SignInWidget(),
     );
   }
 }
